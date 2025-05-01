faf591e4dcc8dd9a2f61e4d06b1fda3f99ddea4f
cleme <clement.lrd@hotmail.fr>
2025-05-01 14:38:02 +0200

    fix found_number / add PGCD function

--- python/found_number.py
+++ python/found_number.py
@@ -1,20 +1,27 @@
 import random
 import os
 
-nbr = random.seed(99)
+def clear():
+    os.system("clear")
+
+nbr = random.randrange(1, 99)
 user_nbr = ""
 indice = 100
 
+clear()
 while user_nbr != nbr:
     print("Devinez le nombre secret !\n")
-    print(f"Indice : c'est moins que {indice}") if nbr =< indice else print(f"Indice : c'est plus que {indice}")
-    user_nbr = input("Entrez votre reponse ici: \n").strip()
+    print(f"Indice : c'est moins de {indice}") if nbr <= indice else print(f"Indice : c'est plus de {indice}")
+    user_nbr = input("\nEntrez votre reponse ici: ").strip()
     try:
         user_nbr = int(user_nbr)
         if user_nbr != nbr:
-            print("Eh non, essayez encore!")
+            clear()
+            print("Eh non, essayez encore")
             indice = user_nbr
     except:
+        clear()
         print("Il faut deviner un nombre\n")
+clear()
 print(f"Bravo, le nombre secret est bien {user_nbr}, vous avez gagné")
 input()
